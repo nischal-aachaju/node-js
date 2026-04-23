ff2fe3c8c8f1c4d398d466ea739d9952ef2bd9eb
Nischal Shrestha <sthanischal5060@gmail.com>
2026-04-23 23:15:55 +0545

    params ans query

--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
-const express = require("express");
-const app = express();
+const express=require("express");
+const app=express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello World!" });
-});
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.get("/",(req,res)=>{
+    let code="<h1>Hello, my name is nischal</h1>"
+
+    res.send(code);
+})
+
+// app.get("/:username",(req,res)=>{
+//     let {username}=req.params;
+//     res.send(`<h1>Hello, ${username}</h1>`);
+// })
+
+app.get("/search",(req,res)=>{
+    // res.send("<h1>hello</h1>")
+    // console.log(req.query);
+    let {q}=req.query;
+    if (!q){
+       res.send(`<h1>nothing search</h1>`); 
+    }
+    res.send(`<h1>you search ${q}</h1>`);
+})
+app.post("/user",(req,res)=>{
+    res.send("Hello World!");
+
+    req.body.name="Nischal";
+    res.send(req.body);
+
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
